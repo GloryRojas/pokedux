996af0e96079234ecb804711b5f13e49a5097890
Glory Rojas <grojasmendoza@proveedor.teco.com.ar>
2022-02-21 18:12:45 -0500

    add: service to call pokemons data

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Searcher from '../../components/Searcher';
 import './styles.css';
 import PokemonList from "../../components/PokemonList";
+import {getPokemons} from "../../api/getPokemons";
 
 function Home() {
+    useEffect(() => {
+        getPokemons(20).then(e => console.log(e));
+    }, [])
   return (
     <div className='Home'>
       <Searcher />
